c7974a966b37ab25a835c8fa257af0612a46c74e
MichaelB167 <michael.m.bates@gmail.com>
2016-04-27 11:50:17 -0400

    Created article and models specs

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,4 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Comment do
+
+  def association
+    described_class.reflect_on_association(:article)
+  end
+
+  it 'belongs to an article' do
+    expect(association).to_not be_nil
+    expect(association.name).to eq(:article)
+  end
+
+  it 'has a set inverse_of record' do
+    expect(association.options[:inverse_of]).to_not be_nil
+    expect(association.options[:inverse_of]).to eq(:comments)
+  end
+
+  it 'validates the presence of comment\'s content' do
+    expect(Comment.create(content: 'Comment Stuff')).to be_valid
+    expect(Comment.create(content: '')).to be_invalid
+  end
 end
